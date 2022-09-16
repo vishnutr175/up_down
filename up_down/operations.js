976d38b599bbcb48701958af1bd2fe27fb593ee7
Vishnu TR <96156034+vishnutr175@users.noreply.github.com>
2022-09-16 11:45:31 -0700

    Add files via upload

---
+++ up_down/operations.js
@@ -0,0 +1,18 @@
+var show=document.getElementById("screen");
+
+function button_click1(value){
+    show.value++;
+
+
+}
+function button_click3(value){
+    show.value--;
+
+
+}
+function  button_click2(){
+    show.value="";
+
+}
+
+
